5a542ef1d791a31a09ec4c741eabea6cd5a2e533
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-01-07 20:24:19 +0900

    add __init__.py

---
+++ g2bp/__main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+def main():
+    args = parse_args()
+    function = functions[args.function]
+    try:
+        function(*args.args)
+    except TypeError as e:
+        print(e)
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("function", choices=functions)
+    parser.add_argument("args", type=str, nargs="*")
+    args = parser.parse_args()
+    return args
+
+functions = {}
+
+if __name__ == "__main__":
+    main()
